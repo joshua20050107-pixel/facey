15f10bd3460408dc04cb61eaff04a87f361b99fd
joshua shindou <joshua20050107@icloud.com>
2026-02-21 00:32:15 +0900

    “asd”

--- lib/screens/scan_tab_screen.dart
+++ lib/screens/scan_tab_screen.dart
@@ -537,7 +537,7 @@ class _ScanTabScreenState extends State<ScanTabScreen> {
           child: Column(
             children: [
               const TopHeader(
-                title: 'ビジュアル評価',
+                title: '魅力を測定',
                 titleStyle: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.w900,
